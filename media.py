dbd1b3eba92421747d337b1a7dab59f3768e0ebc
Chengcheng2017 <cw2964@tc.columbia.edu>
2018-04-21 11:13:58 -0700

    Update media.py

--- media.py
+++ media.py
@@ -2,13 +2,15 @@ import webbrowser
 
 
 class Movie:
-    # This provides documentation about the class "Movie"
-    # that stores movie related information.
-    # Attributes:
-    # title: A string to store the title of the movie.
-    # storyline: A string to store the basic plot of the movie.
-    # poster_image_url: A string to store the URL of the movie poster.
-    # trailer_youtube_url: A string to store the URL of the movie trailer.
+    """This provides documentation about the class "Movie"
+    that stores movie related information.
+
+    Attributes:
+        title: A string to store the title of the movie.
+        storyline: A string to store the basic plot of the movie.
+        poster_image_url: A string to store the URL of the movie poster.
+        trailer_youtube_url: A string to store the URL of the movie trailer.
+    """
     """ This class provides a way to store movie related information """
     # Initializing and creating space in memory
     def __init__(self, movie_title, movie_storyline,
